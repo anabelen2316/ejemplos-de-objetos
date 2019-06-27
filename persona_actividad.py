3d78fc9c89d45197f07a38e79646c48fbfb52bec
anabelen2316 <52163970+anabelen2316@users.noreply.github.com>
2019-06-27 13:53:56 +0100

    revisando lo que tengo

--- persona_actividad.py
+++ persona_actividad.py
@@ -5,34 +5,32 @@ class Persona(object):
     nombre = ""
 
     def corre(self):
-       return ("Puede correr")
+       return ("Puede correr\n")
     def salta(self):
-        return ("Puede saltar")
+        return ("Puede saltar\n")
     def camina(self):
-        return ("Puede caminar")
+        return ("Puede caminar\n")
 
 
 
 
 class Reloj(object):
     hora = 0
-    mimuto = 0
+    minuto = 0
     seg = 0
 
     def dame_hora(self):
-        return str("son las" + str(self.hora)+ str(self.mimuto)+ str(self.seg)+ ":")
+        return str("son las" + str(self.hora) + str(self.minuto) + str(self.seg) + ":")
 
 
-    def horaCorecta(self):
+    def horaCorrecta(self):
         if 0<= self.hora <=23:
-            if 0<= self.mimuto <=60:
+            if 0<= self.minuto <=60:
                 if 0<= self.seg <=60:
                     return True
         else:
             print("Pusiste mal la hora")
             return False
-
-
 
 
 
--- principal.py
+++ principal.py
@@ -6,59 +6,59 @@ from persona_actividad import *
 
 
 
-et = Objeto()
-print(et.color)
-print(et.tamanio)
-print(et.aspecto)
-et.color = "rosa"
-print(et.color)
-
-
-
-"""objeto = MiClase()
-print objeto.propiedad
-objeto.otra_propiedad = "Nuevo valor"
-variable = objeto.metodo()
-print variable
-print objeto.otro_metodo() """
-
-
-ob = Pie()
-ob.color = "El pie de de color verde"
-ob.forma ="El pie es gordito"
-ob.amputar()
-ob.amputar()
-print("te quedaste sin deditos huevon")
-print(ob.color)
-print(ob.forma)
-print(ob.dedos)
-
-
-
-Prueba = Romano(950)
-print(Prueba.romanizar())
-
-
-
-#factura
-compra1 = Factura(12,110)
-print(compra1.unidad)
-print(compra1.precio)
-print(compra1.a_pagar(),"euros")
-print(Factura._tasas) #error
-#atribiteError:type object "factura" has no attribute "_tasa"
-
-
-#empleado
-empleado1 = Empleado("Fancisco",30000)
-print(empleado1.getnombre())
-empleado1.setnombre("Francisco jose")
-print(empleado1.setnombre(),",",empleado1.getsalario())
+# et = Objeto()
+# print(et.color)
+# print(et.tamanio)
+# print(et.aspecto)
+# et.color = "rosa"
+# print(et.color)
+#
+#
+#
+# """objeto = MiClase()
+# print objeto.propiedad
+# objeto.otra_propiedad = "Nuevo valor"
+# variable = objeto.metodo()
+# print variable
+# print objeto.otro_metodo() """
+#
+#
+# ob = Pie()
+# ob.color = "El pie de de color verde"
+# ob.forma ="El pie es gordito"
+# ob.amputar()
+# ob.amputar()
+# print("te quedaste sin deditos huevon")
+# print(ob.color)
+# print(ob.forma)
+# print(ob.dedos)
+#
+#
+#
+# # Prueba = Romano(950)
+# # print(Prueba.romanizar())
+#
+#
+#
+# #factura
+# compra1 = Factura(12,110)
+# print(compra1.unidad)
+# print(compra1.precio)
+# print(compra1.a_pagar(),"euros")
+# print(Factura._tasas) #error
+# #atribiteError:type object "factura" has no attribute "_tasa"
+#
+#
+# #empleado
+# empleado1 = Empleado("Fancisco",30000)
+# print(empleado1.getnombre())
+# empleado1.setnombre("Francisco jose")
+# print(empleado1.setnombre(),",",empleado1.getsalario())
 
 
 #creas una clase de personas
 nuevo_nombre = Persona()
-nuevo_nombre.nombre = "esteban"
+nuevo_nombre.nombre = "Esteban"
 nuevo_nombre.edad = 25
 print(nuevo_nombre.nombre)
 print(nuevo_nombre.edad)
@@ -66,10 +66,10 @@ print(nuevo_nombre.corre()+ nuevo_nombre.salta()+nuevo_nombre.camina())
 
 
 
-#rejoz
-relojito = Reloj():
+#reloj
+relojito = Reloj()
 print(relojito.dame_hora())
-print(horaCorrecta())
+print(relojito.horaCorrecta())
 
 
 
