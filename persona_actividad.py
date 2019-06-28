5bcc7666719696b311402d1c1312545c94c63fbf
anabelen2316 <52163970+anabelen2316@users.noreply.github.com>
2019-06-28 09:30:29 +0100

    ejercicios terminados

--- persona_actividad.py
+++ persona_actividad.py
@@ -20,20 +20,35 @@ class Reloj(object):
     seg = 0
 
     def dame_hora(self):
-        return str("son las" + str(self.hora) + str(self.minuto) + str(self.seg) + ":")
+        return str("son las " + str(self.hora) + str(self.minuto) + str(self.seg) + ": ")
 
 
-    def horaCorrecta(self):
-        if 0<= self.hora <=23 and :
-            if 0<= self.minuto <=60:
-                if 0<= self.seg <=60:
-                    return True
+    def horaCorrecta(self,hr, mim, segundo):
+        if str(hr).isdigit() and str(mim).isdigit() and str(segundo).isdigit():
+            if 0<= hr <=23:
+                if 0<= mim <=60:
+                    if 0<= segundo <=60:
+                        return True
+                    else:
+                        print("Pusiste mal los segundos")
+                        return False
+                else:
+                    print("Pusiste mal los minutos")
+                    return False
+            else:
+                print("Pusiste mal la hora")
+                return False
         else:
-            print("Pusiste mal la hora")
+            print("Tienes que ponener numeritos huevon ")
             return False
 
 
 
+    def sethora(self,hr, mim, segundo):
+        if self.horaCorrecta(hr, mim, segundo):
+            self.hora = hr
+            self.minuto = mim
+            self.seg = segundo
 
 
 
--- principal.py
+++ principal.py
@@ -5,7 +5,7 @@ from empleado import *
 from persona_actividad import *
 
 
-
+#
 # et = Objeto()
 # print(et.color)
 # print(et.tamanio)
@@ -32,14 +32,14 @@ from persona_actividad import *
 # print(ob.color)
 # print(ob.forma)
 # print(ob.dedos)
-#
-#
-#
-# # Prueba = Romano(950)
-# # print(Prueba.romanizar())
-#
-#
-#
+
+
+
+# Prueba = Romano(950)
+# print(Prueba.romanizar())
+
+
+
 # #factura
 # compra1 = Factura(12,110)
 # print(compra1.unidad)
@@ -47,29 +47,29 @@ from persona_actividad import *
 # print(compra1.a_pagar(),"euros")
 # print(Factura._tasas) #error
 # #atribiteError:type object "factura" has no attribute "_tasa"
-#
-#
-# #empleado
+
+
+#empleado
 # empleado1 = Empleado("Fancisco",30000)
 # print(empleado1.getnombre())
 # empleado1.setnombre("Francisco jose")
 # print(empleado1.setnombre(),",",empleado1.getsalario())
 
-
-#creas una clase de personas
-nuevo_nombre = Persona()
-nuevo_nombre.nombre = "Esteban"
-nuevo_nombre.edad = 25
-print(nuevo_nombre.nombre)
-print(nuevo_nombre.edad)
-print(nuevo_nombre.corre()+ nuevo_nombre.salta()+nuevo_nombre.camina())
+#
+# #creas una clase de personas
+# nuevo_nombre = Persona()
+# nuevo_nombre.nombre = "Esteban"
+# nuevo_nombre.edad = 25
+# print(nuevo_nombre.nombre)
+# print(nuevo_nombre.edad)
+# print(nuevo_nombre.corre()+ nuevo_nombre.salta()+nuevo_nombre.camina())
 
 
 
 #reloj
 relojito = Reloj()
 print(relojito.dame_hora())
-print(relojito.horaCorrecta())
+print(relojito.horaCorrecta(2,5,90))
 
 
 
