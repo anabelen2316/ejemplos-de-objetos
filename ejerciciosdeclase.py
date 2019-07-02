c043cbfe83bb30ec7e03079837ba40c96ef09f75
anabelen2316 <52163970+anabelen2316@users.noreply.github.com>
2019-07-02 10:36:20 +0100

    hemos echo un nuevo ejercicio

---
+++ ejerciciosdeclase.py
@@ -0,0 +1,28 @@
+""" El alumnado tendrá que realizar un programa que conste de una clase
+llamada Alumno que tenga como atributos el nombre y la nota del alumno.
+Definir los métodos para inicializar sus atributos, imprimirlos y mostrar
+un mensaje con el resultado de la nota y si ha aprobado o no."""
+
+
+
+
+
+
+
+class Alumnos(object):
+    nombre = ""
+    nota = 0
+
+    def get_nombre(self):
+        return self.nombre
+    def get_nota(self):
+        return self.nota
+    def set_nombre(self,apodo):
+        self.nombre = apodo
+    def set_nota(self,clasificacion):
+        self.nota = clasificacion
+    def aprobarNota(self):
+        if self.nota >=5:
+            print("este alumno ha aprobado")
+            return ("El Alumno ha superado el cursos, Feliz fin de Curso")
+
---
+++ prinactividadclase.py
@@ -0,0 +1,9 @@
+from ejerciciosdeclase import *
+
+
+
+
+pepe = Alumnos()
+pepe.set_nombre("jose luis")
+pepe.set_nota(9)
+
